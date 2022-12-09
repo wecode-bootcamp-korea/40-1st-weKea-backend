f92631bfec414ceaa3b060004d6d60ec3e881961
HyungJoonYoon <hysy9255@gmail.com>
2022-12-09 18:05:33 +0900

    health check를 위한 /ping 앤드 포인트 삭제 완료

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,10 +2,6 @@ const express = require("express");
 const authController = require("../controllers/auth.controller");
 const authRouter = express.Router();
 
-authRouter.get("/ping", (req, res) => {
-  res.status(200).json({ message: "pong" });
-});
-
 authRouter.post("/signUp", authController.signUp);
 authRouter.post("/signIn", authController.signIn);
 
