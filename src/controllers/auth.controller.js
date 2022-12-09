0a2cdc51cc92c5a99363827a314471e2c7b07baf
HyungJoonYoon <hysy9255@gmail.com>
2022-12-09 18:24:47 +0900

    signUp & signIn API에 에러 헨들링 적용 완료

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,32 @@
 const authService = require("../services/auth.service");
 
 const signUp = async (req, res) => {
-  const { name, birthdate, phoneNumber, gender, address, email, password } =
-    req.body;
-  await authService.signUp(
-    name,
-    birthdate,
-    phoneNumber,
-    gender,
-    address,
-    email,
-    password
-  );
-  res.status(201).end();
+  try {
+    const { name, birthdate, phoneNumber, gender, address, email, password } =
+      req.body;
+    await authService.signUp(
+      name,
+      birthdate,
+      phoneNumber,
+      gender,
+      address,
+      email,
+      password
+    );
+    res.status(201).end();
+  } catch (err) {
+    res.status(err.statusCode || 400).json({ message: err.message });
+  }
 };
 
 const signIn = async (req, res) => {
-  const { email, password } = req.body;
-  const accessToken = await authService.signIn(email, password);
-  res.status(200).json({ accessToken: accessToken });
+  try {
+    const { email, password } = req.body;
+    const accessToken = await authService.signIn(email, password);
+    res.status(200).json({ accessToken: accessToken });
+  } catch (err) {
+    res.status(err.statusCode || 401).json({ message: err.message });
+  }
 };
 
 module.exports = { signUp, signIn };
