c708a525d62b893d819506f823df2561f73ee1fe
HyungJoonYoon <hysy9255@gmail.com>
2022-12-12 18:19:59 +0900

    Controllers 단에서 signIn 함수 내부에 exception handling 추가

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,8 +37,13 @@ const signUp = async (req, res) => {
 const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const accessToken = await authService.signIn(email, password);
-    res.status(200).json({ accessToken: accessToken });
+
+    if (!email || !password) {
+      throw new Error("USER_INFORMATION_MISSING");
+    } else {
+      const accessToken = await authService.signIn(email, password);
+      res.status(200).json({ accessToken: accessToken });
+    }
   } catch (err) {
     res.status(err.statusCode || 401).json({ message: err.message });
   }
