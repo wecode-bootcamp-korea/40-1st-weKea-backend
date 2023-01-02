3c0f8a197d5442f82f7ae8701ed803d6da35a7a3
HyungJoonYoon <hysy9255@gmail.com>
2023-01-02 17:39:19 +0900

    수정

--- src/utils/loginRequired.js
+++ src/utils/loginRequired.js
@@ -1,17 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 const verifyUser = async (req, res, next) => {
-  const { jwtoken } = req.headers;
   try {
-    await jwt.verify(
-      jwtoken,
-      process.env.SECRETE_KEY,
-      (err, decoded_payload) => {
-        if (decoded_payload != undefined) {
-          req.decodedJwtPayload = decoded_payload;
-        }
-      }
-    );
+    const jwtoken = req.headers.authorization;
+
+    if (jwtoken != undefined) {
+      const decoded = await jwt.verify(jwtoken, process.env.SECRETE_KEY);
+      req.decodedJwtPayload = decoded;
+    }
   } catch (err) {
     res.status(err.StatusCode || 400).json({ message: err.message });
   }
